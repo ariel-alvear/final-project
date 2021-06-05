23c70c3bbae812762548953b208739f129e9cb1a
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-06-05 13:07:14 -0400

    edit user with ajax modal

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,6 +2,5 @@ $(document).ready(function() {
   $('#trigger-show-modal').click(function(event) {
     event.preventDefault();
     $('#modal-user-show').modal('show');
-    $('#close-modal-user').modal('hide');
   })
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,12 @@ class UsersController < ApplicationController
     @user.document_id = params["user"]["document_id"]
     @user.profesion_id = params["user"]["profesion_id"] unless @user.profile == 1
     @user.email = params["user"]["email"]
+    
+    respond_to do |format|
+      if @user.save
+        format.js
+      end
+    end
   end
+
 end
